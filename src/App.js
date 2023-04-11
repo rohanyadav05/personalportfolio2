fb88395452ab7d70f05e40315a9b3fe6a6dedd87
java13017037 <rohan.prakash.yadav@accenture.com>
2023-04-12 03:43:24 +0530

    added whole directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+//import "..node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Home from './web_components/Home';
+import About from './web_components/About'
+import Service from './web_components/Service'
+import Contact from './web_components/Contact'
+import Navbar from './web_components/Navbar';
+import Footer from './web_components/Footer';
+import History from './web_components/History';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <BrowserRouter >
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/About" element={<About />} />
+    <Route path="/Service" element={<Service />} />
+    <Route path="/Contact" element={<Contact />} />
+    <Route path="/History" element={<History/>} />
+    {/* <Route>
+        <Redirect to="/" />
+      </Route> */}
+    </Routes>
+   
+    
+
+   </BrowserRouter>
+   <Footer/>
+   
+   </>
   );
 }
 
 export default App;
+
